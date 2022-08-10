6fd0dcc7129c8a668d316d8c35a30a09218a5f71
tyulyukov <makstyulyukov@gmail.com>
2022-08-10 03:27:02 +0300

    Small refactoring

--- src/stores/api.js
+++ src/stores/api.js
@@ -2,8 +2,8 @@ import { defineStore } from 'pinia';
 
 export const useApiStore = defineStore('api', {
     state: () => ({
-        serverUrl: 'http://localhost:4224/',
-        /*serverUrl: 'https://server-echo.herokuapp.com/',*/
+        /*serverUrl: 'http://localhost:4224/',*/
+        serverUrl: 'https://server-echo.herokuapp.com/',
         jwt: localStorage.getItem('jwt') || null
     }),
     getters: {
